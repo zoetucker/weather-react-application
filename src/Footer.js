2997580ce298204f0ba576c8b0049c2875b74bad
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-05-25 23:34:38 -0400

    added hosting on Netlify

--- src/Footer.js
+++ src/Footer.js
@@ -6,14 +6,21 @@ export default function Footer() {
   return (
     <div className="Footer">
       <h6>
-        This application is coded by ©Zoe Tucker 💁💻 <br /> and opened-source
-        on{" "}
+        This application is coded by ©Zoe Tucker 💁💻 <br /> Opened-source on{" "}
         <a
           href="https://github.com/zoetucker/weather-react-application"
           target="_blank"
           rel="noreferrer"
         >
           GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://cocky-bhabha-b5346f.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
         </a>
       </h6>
     </div>
